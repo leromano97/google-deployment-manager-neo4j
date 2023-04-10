c8e5484cb7e95225459c14d7d7f01e47318a16e8
leromano97 <lucas.romano@mercadolibre.com>
2023-04-10 09:22:13 -0300

    turn off external ip and change network configW

--- scripts/neo4j-enterprise/network.py
+++ scripts/neo4j-enterprise/network.py
@@ -1,29 +1,23 @@
 def generate_config(context):
-    region = context.properties['region']
-    prefix = context.env['deployment']
+    region = 'us-east4'
 
-    network_name = prefix + '-network'
-    subnet_name = prefix + '-subnetwork'
+    network_name = 'fury'
+    subnet_name = 'da-neo4j'
 
     network_ref = '$(ref.{}.selfLink)'.format(network_name)
     subnet_ref = '$(ref.{}.selfLink)'.format(subnet_name)
-    subnet_cidr = '10.128.0.0/20'
+    subnet_cidr = '10.87.96.0/20'
 
     network = {
         'name': network_name,
         'type': 'compute.v1.network',
-        'properties': {
-            'autoCreateSubnetworks': False,
-        }
     }
     subnet = {
         'name': subnet_name,
         'type': 'compute.v1.subnetwork',
         'properties': {
             'region': region,
-            'privateIpGoogleAccess': False,
-            'ipCidrRange': subnet_cidr,
-            'network': network_ref,
+            'network': 'projects/shared-vpc-224614/global/networks/fury'
         }
     }
     config = {'resources': [], 'outputs': []}
